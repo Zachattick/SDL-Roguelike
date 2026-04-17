86d09f9850beb624ee3a4388db16259dcb7ceb3c
Zach <zachery3033@gmail.com>
2026-04-16 17:34:51 -0700

    Cleaner movement, can now press any combination of buttons and it works.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+struct Player {
+    int x, y;
+    int width, height;
+    int x_velocity, y_velocity;
+    SDL_Rect rect;
+};
+
+struct key_state {
+    int w_pressed;
+    int a_pressed;
+    int s_pressed;
+    int d_pressed;
+};
+
 int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -11,8 +25,9 @@ int main(void)
     SDL_Event event;
     int running = 1;
     
-    SDL_Rect player = { 100, 100, 50, 50 };
+    struct Player player = { 100, 100, 50, 50, 0, 0, {100, 100, 50, 50} };
     
+    struct key_state keys = {0, 0, 0, 0};
     int y_velocity = 0;
     int x_velocity = 0;
 
@@ -27,32 +42,52 @@ int main(void)
             {
                 switch (event.key.keysym.sym)
                 {
-                    case SDLK_w: y_velocity = -5; break;
-                    case SDLK_s: y_velocity = 5; break;
-                    case SDLK_a: x_velocity = -5; break;
-                    case SDLK_d: x_velocity = 5; break;
+                    case SDLK_w: keys.w_pressed = 1; break;
+                    case SDLK_s: keys.s_pressed = 1; break;
+                    case SDLK_a: keys.a_pressed = 1; break;
+                    case SDLK_d: keys.d_pressed = 1; break;
                 }
             }
             else if (event.type == SDL_KEYUP)
             {
                 switch (event.key.keysym.sym)
                 {
-                    case SDLK_w: y_velocity = 0; break;
-                    case SDLK_s: y_velocity = 0; break;
-                    case SDLK_a: x_velocity = 0; break;
-                    case SDLK_d: x_velocity = 0; break;
+                    case SDLK_w: keys.w_pressed = 0; break;
+                    case SDLK_s: keys.s_pressed = 0; break;
+                    case SDLK_a: keys.a_pressed = 0; break;
+                    case SDLK_d: keys.d_pressed = 0; break;
                 }
             }   
         }
         
+
+        if (keys.w_pressed) 
+        {
+            if (keys.s_pressed) y_velocity = 0;
+            else y_velocity = -5;
+        }
+        else if (keys.s_pressed) y_velocity = 5;
+        else y_velocity = 0;
+
+        if (keys.a_pressed)
+        {
+            if (keys.d_pressed) x_velocity = 0;
+            else x_velocity = -5;
+        }
+        else if (keys.d_pressed) x_velocity = 5;
+        else x_velocity = 0;
+
         player.x += x_velocity;
         player.y += y_velocity;
+
+        player.rect.x = player.x;
+        player.rect.y = player.y;
 
         SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
         SDL_RenderClear(renderer);
         
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &player);
+        SDL_RenderFillRect(renderer, &player.rect);
 
         
         SDL_RenderPresent(renderer);
